22dfe196d5ffe3b600f32deb163d678f35e763ef
Jonathan Batalla <batallajjet@outlook.com>
2022-11-14 10:56:58 -0600

    changed deck 2

--- src/components/Game/Deck.js
+++ src/components/Game/Deck.js
@@ -1,35 +1,66 @@
-const suits = ["coin", "sword", "club", "cup"];
-const value = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
+// const suits = ["coin", "sword", "club", "cup"];
+// const value = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
 
-const makeDeck = (rank, value) => {                                            // Creation of deck of cards
-    const card = {
-      rank: rank,
-      value: value
-    };
-    deck.push(card);
-  };
+// const makeDeck = (value, suit) => {                                            // Creation of deck of cards
+//     const card = {
+//       value: value,
+//       suit: suit
+//     };
+//     deck.push(card);
+//   };
   //--------------------------------------------------------------------------------------------------------------
-  function shuffle(deck) {                                                      // Shuffles deck
-    let i = deck.length
-    let ri;
-  
-    while (i != 0) {
-      ri = Math.floor(Math.random()*i);
-      i--;
-  
-      [deck[i], deck[ri]] =  [deck[ri], deck[i]];
-    }
-  }
-  //--------------------------------------------------------------------------------------------------------------
-  function getDeck() {                                                          // Passes parameters of rank and suit
-  deck = []
-  for (const suit of suits) {                                                     // of a card
-    for (const value of value) {
-      makeDeck(rank, suit);
-    }
-  }
-//   shuffle(deck);
-  return deck
-  }
+//   function getDeck() {                                                          // Passes parameters of rank and suit
+//   deck = []
+//   for (const suit of suits) {                                                     // of a card
+//     for (const valu of value) {
+//       makeDeck(valu, suit);
+//     }
+//   }
+// //   shuffle(deck);
+//   return deck
+//   }
 
-  console.log(getDeck())
+
+
+let gamedeck = [
+    { value: 1, suit: 'coin' },
+    { value: 2, suit: 'coin' },
+    { value: 3, suit: 'coin' },
+    { value: 4, suit: 'coin' },
+    { value: 5, suit: 'coin' },
+    { value: 6, suit: 'coin' },
+    { value: 7, suit: 'coin' },
+    { value: 10, suit: 'coin' },
+    { value: 11, suit: 'coin' },
+    { value: 12, suit: 'coin' },
+    { value: 1, suit: 'sword' },
+    { value: 2, suit: 'sword' },
+    { value: 3, suit: 'sword' },
+    { value: 4, suit: 'sword' },
+    { value: 5, suit: 'sword' },
+    { value: 6, suit: 'sword' },
+    { value: 7, suit: 'sword' },
+    { value: 10, suit: 'sword' },
+    { value: 11, suit: 'sword' },
+    { value: 12, suit: 'sword' },
+    { value: 1, suit: 'club' },
+    { value: 2, suit: 'club' },
+    { value: 3, suit: 'club' },
+    { value: 4, suit: 'club' },
+    { value: 5, suit: 'club' },
+    { value: 6, suit: 'club' },
+    { value: 7, suit: 'club' },
+    { value: 10, suit: 'club' },
+    { value: 11, suit: 'club' },
+    { value: 12, suit: 'club' },
+    { value: 1, suit: 'cup' },
+    { value: 2, suit: 'cup' },
+    { value: 3, suit: 'cup' },
+    { value: 4, suit: 'cup' },
+    { value: 5, suit: 'cup' },
+    { value: 6, suit: 'cup' },
+    { value: 7, suit: 'cup' },
+    { value: 10, suit: 'cup' },
+    { value: 11, suit: 'cup' },
+    { value: 12, suit: 'cup' }
+  ]
