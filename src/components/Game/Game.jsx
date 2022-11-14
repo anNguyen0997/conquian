5690dbb3595cf8509666563634f108d5b15026ff
Jonathan Batalla <batallajjet@outlook.com>
2022-11-14 10:46:46 -0600

    change deck

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,11 +1,25 @@
 import { gamedeck } from './Deck'
 
+function shuffle(deck) {                                                      // Shuffles deck
+    let i = deck.length
+    let ri;
+  
+    while (i != 0) {
+      ri = Math.floor(Math.random()*i);
+      i--;
+  
+      [deck[i], deck[ri]] =  [deck[ri], deck[i]];
+    }
+    return deck
+  }
+
+
 const Game = {
     name: "Conquian",
 
-  setup: () => ({
-    deck: gamedeck
-  }),
+    setup: () => ({
+        deck: shuffle(gamedeck)
+    }),
 
 
   
