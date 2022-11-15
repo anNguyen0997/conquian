1fbf2dba46c75126dd7043a3656aa5adf7f6c35e
Khanh Trinh <kqtrinh87@gmail.com>
2022-11-15 10:16:10 -0600

    Changed header, added footer, mobile-friendly

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function Footer() {
+export default function footer() {
   return (
-    <div>Footer</div>
+    <footer className="bg-white fixed inset-x-0 bottom-0 hidden lg:block">
+      <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
+        <div className="mt-8 flex justify-center space-x-6">
+        </div>
+        <p className="mt-8 text-center text-base text-gray-400">"Team Astro" from DigitalCrafts, Cohort of August, 2022</p>
+      </div>
+    </footer>
   )
 }
-
-export default Footer
