ac20f9b69969bc02daedede430918e49571f40ff
Jonathan Batalla <batallajjet@outlook.com>
2022-11-17 13:24:54 -0600

    added css table

--- src/components/Game/Board.jsx
+++ src/components/Game/Board.jsx
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
-import Deck from './Deck'
+import GameArea from './BoardComp/GameArea';
 import './Board.css'
 import PropTypes from "prop-types";
 import Player from "./BoardComp/Player";
 
 
 export default class Board extends Component {
+
+
+
   render() {
     return (
       <div id='Game'>
-        <div id='Table'>
 
-            <div id='Deck'>
-              <Deck card={this.props.G.stageCard}/>
-            </div>
+        <div id='TableBorder'>
+          <div id='TableInline'>
 
-            <div id='foe'>
-              <Player player={this.props.G.players[2]} />
-            </div>
-
-            <div id='foe'>
-              <Player player={this.props.G.players[1]} />
+            <div id='gameArea'>
+              <GameArea {...this.props} />
             </div>
 
             <div id='player'>
-              <Player player={this.props.G.players[0]}/>
+              <Player player={this.props.G.players[this.props.playerID]} moves={this.props.moves}/>
             </div>
 
+
+          </div>
         </div>
+        
     </div>
     )
   }
@@ -39,4 +39,3 @@ Board.propTypes = {
   moves: PropTypes.object,
   playerID: PropTypes.string,
 };
-
