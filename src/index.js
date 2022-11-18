3b08ab33f8bb93773e2b1c48213e83347803090b
Khanh Trinh <kqtrinh87@gmail.com>
2022-11-18 10:47:23 -0600

    Removed Redux, fixed local compiling issue

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import {createStore} from 'redux'
-import reducer from './reducer/reducer';
-import { Provider } from 'react-redux';
 import "./index.css"
 import BaseLayout from './components/layout/BaseLayout';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 
-const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store} className="flex flex-col min-h-screen">
-        <BaseLayout />
-    </Provider>
+
+        <BaseLayout className="flex flex-col min-h-screen"/>
+
     </BrowserRouter>
   </React.StrictMode>
 );
