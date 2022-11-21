e888a52da3088b98930670a68ae90933c8ffa95e
Khanh Trinh <kqtrinh87@gmail.com>
2022-11-21 11:02:16 -0600

    Added Home Page Decor

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <footer className="bg-white fixed inset-x-0 bottom-0 hidden lg:block">
+    <footer className="bg-transparent fixed inset-x-0 bottom-0 hidden lg:block">
       <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center space-x-6">
         </div>
-        <p className="text-center text-base text-gray-400">"Team Astro" from DigitalCrafts, Cohort of August, 2022</p>
+        <p className="text-center text-base text-gray-400">"Team Rocket" from DigitalCrafts, Cohort of August, 2022</p>
       </div>
     </footer>
   )
