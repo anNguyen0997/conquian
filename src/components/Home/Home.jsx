afaed3b7cf97870e3eceb5bac8e6b022098ee090
An Nguyen <roryn4545@gmail.com>
2022-11-21 11:08:14 -0600

    added update functions

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,21 +3,12 @@ import axios from 'axios'
 
 function Home() {
 
-  const [email, setEmail] = useState('')
-  const [newValue, setNewValue] = useState(0)
-
-  const handleIncrement = (email) => {
-    axios.put('http://localhost:3001/updategameplayed', { gamesplayed: newValue, email: email })
-    .then((response) => {
-      console.log('updated')
-    })
-  }
-
+  
 
   return (
     <>
-      <input type='text' onChange={(e) => {setEmail(e.target.value)}}></input>
-      <button onClick={handleIncrement(email)}>update</button>
+     
+      
     </>
   );
 }
