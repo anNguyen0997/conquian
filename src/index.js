4f85994f17c47160b8aba96c4f8565982a807d94
An Nguyen <roryn4545@gmail.com>
2022-11-21 10:11:31 -0600

    added provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import "./index.css"
 import BaseLayout from './components/layout/BaseLayout';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './reducer/reducer';
 
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
+
+      <BrowserRouter>
         <BaseLayout className="flex flex-col min-h-screen"/>
-    </BrowserRouter>
+      </BrowserRouter>
+
+    </Provider>
   </React.StrictMode>
 );
